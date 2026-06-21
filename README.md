4eb31b3805bcbe1331923c1f57294b796307f05a
FipaLipe <filipi.martinss@hotmail.com>
2026-06-21 20:21:06 -0300

    feat: organize code and explications

---
+++ README.md
@@ -0,0 +1,115 @@
+<!-- @format -->
+
+<img src="./imagens/assets/Cabecalho.png"/>
+
+<h1 align="center">
+Simulando Vida com Partículas
+</h1>
+
+<p align="center">
+  <i>Projeto Final de PCD (Prática em Ciência de Dados)</i><br/>
+  Ilum - Escola de Ciência | Campinas, 2026
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/badge/Python-3.10+-blue">
+  <img src="https://img.shields.io/badge/domain-complex%20systems-green">
+  <img src="https://img.shields.io/badge/focus-emergence-blueviolet">
+</p>
+
+---
+
+# Como executar?
+
+Python 10+ é necessário para rodar o código. o procedimento recomendado é criar um novo ambiente virtual e instalar os módulos necessários com
+
+```sh
+pip install -r requirements.txt
+```
+
+Para rodar a versão final do projeto, execute o arquivo `main.py` com
+
+```sh
+python codigos/main.py
+```
+
+Para entender a base teórica do projeto, leia o arquivo `simulando_vida_com_particulas.ipynb` na pasta _codigos/_.
+
+---
+
+# Sobre o Repositório
+
+Este repositório tem como objetivo visualizar a aparição de `fenômenos emergentes` em sistemas complexos com regras iniciais relativamente simples.
+
+Para isso, foi desenvolvido uma simulação de partículas distintas com interações definidas, inspirado no trabalho de [Clusters de Jeffrey Ventrella](https://www.ventrella.com/Clusters/intro.html). A partir dessa simulação, observa-se a emergência de uma suposta complexidade, com estruturas que se assemelham àquelas observadas na natureza biológica.
+
+# Conteúdos do repositório
+
+<p>
+Este repositório possui um <i>Jupyter Notebook</i> didático que guia o leitor durante o desenvolvimento da simulação, além de um script python com a versão final do projeto.
+</p>
+
+<ul>
+  <li> 📂 <b>codigos</b>: contém os notebooks e arquivos auxiliares utilizados ao longo do projeto</li>
+    <ul>
+      <li> 📂 <b>playground</b>: área de testes para protótipos e funções suporte </li>
+      <li> 📄 <b>main.py</b>: script com a versão final do projeto </li>
+      <li> 📓 <b>simulando_vida_com_particulas.ipynb</b>: Jupyter Notebook que explica a simulação de forma didática <b>(📌começe aqui!)</b></li>
+    </ul>
+  <li> 📂 <b>imagens</b>: contém as imagens utilizadas ao longo do material</li>
+  <ul>
+    <li> 📂 <b>assets</b>: imagens gerais utilizadas para a organização do projeto</li>
+    <ul>
+      <li> 🖼️ <b>Cabecalho.png</b>: imagem utilizada no cabeçalho do README.</li>
+    </ul>
+    <li> 📂 <b>simulando_vida_com_particulas</b>: recursos visuais utilizados no notebook <i>simulando_vida_com_particulas.ipynb</i></li>
+  </ul>
+</ul>
+
+---
+
+# Agradecimentos
+
+Gostaria de agradacer ao estagiário de computação João Vitor Pires de Souza, por estar sempre disponível quando necessário, e ao meu colega de classe Vinícius Marianno de Marque Cutolo por proporcionar bons debates e questionamentos sobre ao projeto.
+
+---
+
+# Professores orientadores
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/drcassar">
+        <img src="https://avatars.githubusercontent.com/u/9871905?v=4" width="100px;" alt="Foto do Daniel no Github" style="border-radius:100%;"/><br>
+        <b>Prof. Dr. Daniel R. Cassar</b>
+      </a>
+    </td>
+    <td align="center">
+      <a href="https://github.com/llemos">
+        <img src="https://avatars.githubusercontent.com/u/1894434?v=4" width="100px;" alt="Foto do Leandro no Github" style="border-radius:100%;"/><br>
+        <b>Prof. Dr. Leandro Nascimento Lemes</b>
+      </a>
+    </td>
+    <td align="center">
+      <a href="https://github.com/jamesmalmeida">
+        <img src="https://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4710448J1" width="100px;" alt="Foto do Daniel no Github" style="border-radius:100%;"/><br>
+        <b>Prof. Dr. James Moraes de Almeida</b>
+      </a>
+    </td>
+  </tr>
+</table>
+
+---
+
+# Autor
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/Fipa-Lipe">
+        <img src="https://avatars.githubusercontent.com/u/90717547?v=4" width="100px;" alt="Foto do Filipi no Github" style="border-radius:100%;"/><br>
+        <b>Filipi Martins</b>
+      </a>
+    </td>
+  </tr>
+</table>
--- codigos/main.py
+++ codigos/main.py
@@ -13,7 +13,52 @@ class Species:
 
 
 class Game:
-    def __init__(self, interactions, force_function, friction, species, particle_number = 300, width = 700, height = 700, max_fps = 60, dt = 1):
+    """Classe base para o jogo
+    
+    Atributos:
+        running (bool): Indica se o jogo está rodando
+        display (pygame.Surface): A superfície de exibição do jogo
+        clock (pygame.time.Clock): Relógio para controlar o FPS
+        width (int): Largura da janela do jogo
+        height (int): Altura da janela do jogo
+        size (tuple): Tamanho da janela do jogo (width, height)
+        max_fps (int): Taxa máxima de quadros por segundo
+        interactions (numpy.ndarray): Matriz de interações entre as espécies de partículas
+        force_function (callable): Função para calcular a força de atração ou repulsão entre as partículas
+        friction (float): Coeficiente de fricção a ser aplicado à velocidade da partícula
+        species (list): Lista de espécies de partículas no jogo
+        particle_number (int): Número de partículas a ser instanciado no jogo
+        particles (dict): Dicionário contendo as informações das partículas, incluindo posições, velocidades e espécies
+        interactions_matrix (numpy.ndarray): Matriz de interações entre as partículas, calculada com base na matriz de interações e nas espécies das partículas
+        interactions_strength (numpy.ndarray): Matriz contendo a força de atração ou repulsão entre as partículas, extraída da matriz de interações
+        interactions_radius (numpy.ndarray): Matriz contendo o raio de visão para as interações entre as partículas, extraída da matriz de interações
+    
+    Métodos:
+        __init__(self, width=700, height=700, max_fps=60): Inicializa os atributos do jogo
+        on_init(self): Configura o ambiente do jogo
+        on_add_particle(self, particle): Adiciona uma partícula à lista de partículas do jogo
+        on_event(self, event): Lida com os eventos do jogo
+        on_loop(self): Atualiza a lógica do jogo
+        on_render(self): Renderiza os elementos do jogo na tela
+        on_cleanup(self): Limpa os recursos do jogo
+        on_execute(self): Executa o loop principal do jogo
+    """
+
+    def __init__(self, interactions, force_function, friction, species, particle_number = 300, width = 700, height = 700, max_fps = 60): 
+        """Inicializa os atributos do jogo.
+
+        Inicializa os atributos do jogo, incluindo as interações, a função de força, o coeficiente de fricção, a largura e altura da janela, a taxa máxima de quadros por segundo, e as variáveis para controlar o estado do jogo e a exibição.
+
+        Args:
+            interactions (numpy.ndarray): A matriz de interações entre as espécies de partículas.
+            force_function (callable): A função para calcular a força de atração ou repulsão entre as partículas.
+            friction (float): O coeficiente de fricção a ser aplicado à velocidade da partícula.
+            species (list): A lista de espécies de partículas no jogo.
+            particle_number (int, optional): O número de partículas a ser instanciado no jogo. Padrão é 300.
+            width (int, optional): Largura da janela do jogo. Padrão é 700.
+            height (int, optional): Altura da janela do jogo. Padrão é 700.
+            max_fps (int, optional): Taxa máxima de quadros por segundo. Padrão é 60.
+        """
         self.running = True
         self.display = None
         self.clock = None
@@ -21,7 +66,6 @@ class Game:
         self.height = height
         self.size = (self.width, self.height)
         self.max_fps = max_fps
-        self.dt = dt
 
         self.interactions = interactions
         self.force_function = force_function
@@ -42,6 +86,13 @@ class Game:
         self.interactions_radius = self.interactions_matrix[:, :, 1]
 
     def initiate_particles(self):
+        """Inicializa as partículas do jogo.
+        
+        Inicializa as partículas do jogo, gerando posições aleatórias dentro dos limites da janela, velocidades iniciais zeradas e atribuindo espécies aleatórias com base na lista de espécies disponível.
+        
+        Returns:
+            dict: Um dicionário contendo as informações das partículas, incluindo posições, velocidades e espécies.
+        """
         particles = {}
         particles["positions"] = np.random.rand(self.particle_number, 2) * [self.width, self.height]
         particles["velocities"] = np.zeros((self.particle_number, 2))
@@ -50,16 +101,35 @@ class Game:
         return particles
 
     def on_init(self):
+        """Configura o ambiente do jogo.
+
+        Configura o ambiente do jogo, inicializando o Pygame, criando a janela de exibição e configurando o relógio para controlar o FPS.
+
+        Returns:
+            bool: Retorna True se a inicialização for bem-sucedida, caso contrário, retorna False.
+        """
         pygame.init()
         self.display = pygame.display.set_mode(self.size)
         self.clock = pygame.time.Clock()
         return True
 
     def on_event(self, event):
+        """Lida com os eventos do jogo.
+        
+        Lida com os eventos do jogo, verificando se o evento é do tipo QUIT para encerrar o jogo.
+        
+        Args:
+            event (pygame.event.Event): O evento a ser processado.
+        """
         if event.type == pygame.QUIT:
             self.running = False
 
     def on_loop(self):
+        """Atualiza a lógica do jogo.
+        
+        Atualiza os elementos do jogo, como a posição dos objetos, e cálculos de interações. Este método é chamado a cada iteração do loop principal do jogo.
+        """
+
         #Atualiza as partículas
         diff = self.particles["positions"][None, :, :] - self.particles["positions"][:, None, :]
         dist = dist = np.linalg.norm(diff, axis=2) + 1e-5 # Soma um número muito pequeno pra não dividir por 0 depois
@@ -77,13 +147,17 @@ class Game:
 
         total_force = np.sum(force, axis=1)
 
-        self.particles["velocities"] = (self.particles["velocities"] + total_force * self.dt) * (1 - self.friction)
+        self.particles["velocities"] = (self.particles["velocities"] + total_force) * (1 - self.friction)
 
-        self.particles["positions"] += self.particles["velocities"] * self.dt
+        self.particles["positions"] += self.particles["velocities"]
         # self.particles["positions"] %= [self.width, self.height]
 
 
     def on_render(self):
+        """Renderiza os elementos do jogo na tela.
+        
+        Renderiza os elementos do jogo na tela, desenhando os objetos e atualizando a exibição. Este método é chamado a cada iteração do loop principal do jogo.
+        """
         self.display.fill((20, 20, 20))
 
         for i in range(len(self.particles["positions"])):
@@ -94,9 +168,17 @@ class Game:
         pygame.display.flip()
 
     def on_cleanup(self):
+        """Limpa os recursos do jogo.
+        
+        Limpa os recursos do jogo, encerrando o Pygame e liberando quaisquer recursos alocados. Este método é chamado quando o jogo é encerrado.
+        """
         pygame.quit()
 
     def on_execute(self):
+        """Executa o loop principal do jogo.
+        
+        Executa o loop principal do jogo, chamando os métodos de inicialização, eventos, lógica e renderização em cada iteração. O loop continua até que a variável 'running' seja definida como False.
+        """
         self.running = self.on_init()
 
         while self.running:
@@ -146,15 +228,15 @@ def force_function(dist, attraction):
 # =============================================
 # SETUP
 # =============================================
-friction = 0.1
+friction = 0.2
 
-red_species = Species(0, (255, 0, 0), 4)
-blue_species = Species(1, (0, 0, 255), 4)
-yellow_species = Species(2, (255, 255, 0), 4)
-green_species = Species(3, (0, 255, 0), 4)
-purple_species = Species(4, (255, 0, 255), 4)
-cyan_species = Species(5, (0, 255, 255), 4)
-white_species = Species(6, (255, 255, 255), 4)
+red_species = Species(0, (255, 0, 0), 2)
+blue_species = Species(1, (0, 0, 255), 2)
+yellow_species = Species(2, (255, 255, 0), 2)
+green_species = Species(3, (0, 255, 0), 2)
+purple_species = Species(4, (255, 0, 255), 2)
+cyan_species = Species(5, (0, 255, 255), 2)
+white_species = Species(6, (255, 255, 255), 2)
 
 species = [
     red_species, blue_species, yellow_species,
@@ -174,6 +256,6 @@ interactions = np.array([
 
 ])
 
-game = Game(interactions, force_function, friction, species, particle_number=600, width=800, height=800)
+game = Game(interactions, force_function, friction, species, particle_number=800, width=800, height=800)
 
 game.on_execute()
